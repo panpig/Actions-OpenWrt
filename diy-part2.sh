39479ab66bde414a0bb508ecb51608548815cabd
panpig <89371886+panpig@users.noreply.github.com>
2021-10-16 15:42:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,9 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/10.10.10.1/192.168.50.254/g' package/base-files/files/bin/config_generate
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=32768' package/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a net.ipv4.neigh.default.gc_thresh3=131702' package/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a sysctl net.ipv4.udp_mem="1024000 1024000 1024000"' package/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a sysctl net.ipv4.udp_rmem_min=51200' package/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a sysctl net.ipv4.udp_wmem_min=51200' package/base-files/files/etc/sysctl.conf
