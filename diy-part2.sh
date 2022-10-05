fb47c6c7ca79f1fbbfce8493128ce9a4f76cdd88
panpig <89371886+panpig@users.noreply.github.com>
2022-10-05 22:22:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_generate
-sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=1000000' package/base-files/files/etc/sysctl.conf
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
