98f33311c09ef79f908d0d58981d585290d6823e
panpig <89371886+panpig@users.noreply.github.com>
2022-10-05 22:27:46 +0800

    Create diy-part64-2.sh

---
+++ diy-part64-2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_generate
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=1000000' package/base-files/files/etc/sysctl.conf
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
+git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
+git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
+git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
