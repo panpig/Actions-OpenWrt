4e56f4a341394ac60c6b618a7a3778fb06423c8a
panpig <89371886+panpig@users.noreply.github.com>
2023-02-23 16:07:10 +0800

    Update diy-part64-2.sh

--- diy-part64-2.sh
+++ diy-part64-2.sh
@@ -9,7 +9,7 @@
 
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_generate
-sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=100000/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=1000000/base-files/files/etc/sysctl.conf
 sed -i '/customized in this file/a net.ipv4.udp_wmem_min=6553500' package/base-files/files/etc/sysctl.conf
 sed -i '/customized in this file/a net.ipv4.udp_rmem_min=6553600' package/base-files/files/etc/sysctl.conf
 sed -i '/customized in this file/a net.core.rmem_max=40960000' package/base-files/files/etc/sysctl.conf
@@ -17,10 +17,7 @@ sed -i '/customized in this file/a net.core.wmem_max=40960000' package/base-file
 sed -i '/customized in this file/a net.core.wmem_default=40960000' package/base-files/files/etc/sysctl.conf
 sed -i '/customized in this file/a net.core.rmem_default=40960000' package/base-files/files/etc/sysctl.conf
 
-# Change default shell to zsh
-# sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
-
-# 移除重复软件包
+ 移除重复软件包
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-netgear
