3e4894be65cc2119dfd323758210497a4a9238e3
panpig <89371886+panpig@users.noreply.github.com>
2023-02-23 16:20:40 +0800

    Update diy-script-64.sh

--- diy-script-64.sh
+++ diy-script-64.sh
@@ -133,5 +133,6 @@ find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/
 sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/controller/*.lua
 sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/*.lua
 sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/view/v2ray_server/*.htm
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
